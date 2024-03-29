f02fc889e28764417e4130bc4b6e3cacabc9409b
DoYam <doniang1123@gmail.com>
2024-03-30 08:50:07 +0900

    Feat: Set Register Page Step 1,2

--- apps/client-user/src/components/Register/RegisterProducts/RegisterProducts.tsx
+++ apps/client-user/src/components/Register/RegisterProducts/RegisterProducts.tsx
@@ -9,60 +9,82 @@ interface RegisterProductsProps {
 }
 
 const RegisterProducts: React.FC<RegisterProductsProps> = ({ step }) => {
+  let title = "";
+  switch (step) {
+    case 1:
+      title = "Register product information";
+      break;
+    case 2:
+      title = "Deploy incentive pool";
+      break;
+    case 3:
+      title = "Get API keys and integrate it";
+      break;
+    case 4:
+      title = "Complete guide";
+      break;
+    default:
+      title = "Unknown step";
+  }
   return (
     <div>
-      <div>
-        <div className="flex flex-row gap-4">
-          <div className="flex flex-col gap-2 align-middle">
-            <Image
-              src={step > 1 ? statusChecked : status}
-              alt="status"
-              width={step > 1 ? 25 : 22}
-              height={step > 1 ? 25 : 22}
-            />
-            <Image src={bar} alt="bar" height={30} />
+      <div className="flex w-[360px] flex-col gap-[60px]">
+        <p className=" pt-[30px] text-20/semi-bold">{title}</p>
+        <div>
+          <div className="flex flex-row gap-4">
+            <div className="flex flex-col gap-2 align-middle">
+              <Image
+                src={step > 1 ? statusChecked : status}
+                alt="status"
+                width={step > 1 ? 25 : 22}
+                height={step > 1 ? 25 : 22}
+              />
+              <Image src={bar} alt="bar" height={30} />
+            </div>
+            <p className={step === 1 ? "text-16/semi-bold text-theme-black" : "text-theme-gray"}>
+              Register product information
+            </p>
           </div>
-          <p className={step === 1 ? "text-16/semi-bold text-theme-black" : "text-theme-gray"}>
-            Register product information
-          </p>
-        </div>
 
-        <div className="mt-2 flex flex-row gap-4">
-          <div className="flex flex-col gap-2 align-middle">
-            <Image
-              src={step > 2 ? statusChecked : status}
-              alt="status"
-              width={step > 2 ? 25 : 22}
-              height={step > 2 ? 25 : 22}
-            />
-            <Image src={bar} alt="bar" height={30} />
+          <div className="mt-2 flex flex-row gap-4">
+            <div className="flex flex-col gap-2 align-middle">
+              <Image
+                src={step > 2 ? statusChecked : status}
+                alt="status"
+                width={step > 2 ? 25 : 22}
+                height={step > 2 ? 25 : 22}
+              />
+              <Image src={bar} alt="bar" height={30} />
+            </div>
+            <p className={step === 2 ? "text-16/semi-bold text-theme-black" : "text-theme-gray"}>
+              Deploy incentive pool
+            </p>
           </div>
-          <p className={step === 2 ? "text-16/semi-bold text-theme-black" : "text-theme-gray"}>Deploy incentive pool</p>
-        </div>
 
-        <div className="mt-2 flex flex-row gap-4">
-          <div className="flex flex-col gap-2 align-middle">
-            <Image
-              src={step > 3 ? statusChecked : status}
-              alt="status"
-              width={step > 3 ? 25 : 22}
-              height={step > 3 ? 25 : 22}
-            />
-            <Image src={bar} alt="bar" height={30} />
+          <div className="mt-2 flex flex-row gap-4">
+            <div className="flex flex-col gap-2 align-middle">
+              <Image
+                src={step > 3 ? statusChecked : status}
+                alt="status"
+                width={step > 3 ? 25 : 22}
+                height={step > 3 ? 25 : 22}
+              />
+              <Image src={bar} alt="bar" height={30} />
+            </div>
+            <p className={step === 3 ? "text-16/semi-bold text-theme-black" : "text-theme-gray"}>
+              Get API keys and integrate it
+            </p>
           </div>
-          <p className={step === 3 ? "text-16/semi-bold text-theme-black" : "text-theme-gray"}>
-            Get API keys and integrate it
-          </p>
-        </div>
 
-        <div className="mt-2 flex flex-row gap-4 align-middle">
-          <Image
-            src={step > 4 ? statusChecked : status}
-            alt="status"
-            width={step > 4 ? 25 : 22}
-            height={step > 4 ? 25 : 22}
-          />
-          <p className={step === 4 ? "text-16/semi-bold text-theme-black" : "text-theme-gray"}>Complete guide</p>
+          <div className="mt-2 flex flex-row gap-4 align-middle">
+            <Image
+              src={step > 4 ? statusChecked : status}
+              alt="status"
+              width={step > 4 ? 25 : 22}
+              height={step > 4 ? 25 : 22}
+            />
+            <p className={step === 4 ? "text-16/semi-bold text-theme-black" : "text-theme-gray"}>Complete guide</p>
+          </div>
         </div>
       </div>
     </div>
--- apps/client-user/src/pages/register/index.tsx
+++ apps/client-user/src/pages/register/index.tsx
@@ -1,15 +1,191 @@
+import clsx from "clsx";
+import { useState } from "react";
+import { Dropdown, TextField } from "@split/ui";
 import RegisterProducts from "../../components/Register/RegisterProducts/RegisterProducts";
 import { Header } from "../../layouts/Common/Header/Header";
 import SubHeader from "../../layouts/Common/SubHeader/SubHeader";
 
 export const Register = () => {
+  const [step, setStep] = useState(1);
+
+  const handleNextStep = () => {
+    if (step < 4) {
+      setStep(step + 1);
+    }
+  };
+
+  const handlePrevStep = () => {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  };
+
+  const getTitleAndSubtitle = (currentStep: number): { title: string; subtitle: string } => {
+    let title: string;
+    let subtitle: string;
+    switch (currentStep) {
+      case 1:
+        title = "First, give us your product’s information.";
+        subtitle =
+          "The product information you provide will be used to help your referrals choose a service, so please take that into consideration.";
+        break;
+      case 2:
+        title = "Next, create your product’s incentive pool.";
+        subtitle = "Create your own incentive pool for your product’s referrers.";
+        break;
+      case 3:
+        title = "Next, integrate the Split SDK to track the transactions.";
+        subtitle =
+          "In order for Split to track transactions made by users on your service, you'll need to integrate the SDK. You can do later with a developer on your team.";
+        break;
+      default:
+        title = "Congratulations!";
+        subtitle = "All steps are completed! Thank you for registering your product with Split.";
+    }
+    return { title, subtitle };
+  };
+
+  const { title, subtitle } = getTitleAndSubtitle(step);
+
+  const options = [
+    { value: "Ethereum", label: "Ethereum" },
+    { value: "ICP", label: "ICP" },
+    { value: "Viction", label: "Viction" },
+  ];
+
+  const renderContentsField = () => {
+    switch (step) {
+      case 1:
+        return (
+          <>
+            <div className="flex flex-col gap-[10px]">
+              <p>Product Name</p>
+              <TextField placeholder="e.g. Split" />
+            </div>
+            <div className="flex flex-col gap-[10px]">
+              <p>Web Link</p>
+              <TextField placeholder="e.g. https://split.io" />
+            </div>
+            <div className="flex flex-col gap-[10px]">
+              <p>Twitter Link</p>
+              <TextField placeholder="e.g. https://twitter.com/split_official" />
+            </div>
+            <div className="flex flex-col gap-[10px]">
+              <p>Product Description</p>
+              <TextField placeholder="e.g. Split is an affiliate marketing service for Web3 apps." />
+            </div>
+            <div className="flex flex-col gap-[10px]">
+              <p>Network</p>
+              <Dropdown options={options} />
+            </div>
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <div className="flex flex-col gap-[10px]">
+              <p>Address for Incentive Token</p>
+              <TextField placeholder="e.g. 0x1a2b3c4d5e6f..." />
+            </div>
+            <div className="flex flex-col gap-[10px]">
+              <p>Incentive Amount for Tx </p>
+              <TextField placeholder="e.g. https://split.io" />
+            </div>
+            <div className="flex flex-col gap-[10px]">
+              <p>Total Txs</p>
+              <TextField placeholder="e.g. https://twitter.com/split_official" />
+            </div>
+          </>
+        );
+      case 3:
+        return <div>get API Keys</div>;
+      case 4:
+        return <div>image</div>;
+      default:
+        return null;
+    }
+  };
+  const renderButtons = () => {
+    switch (step) {
+      case 1:
+        return (
+          <button
+            type="button"
+            className="rounded-[5px] bg-gray-500 px-[30px] py-[16px] text-theme-white"
+            onClick={handleNextStep}
+          >
+            Next
+          </button>
+        );
+      case 2:
+        return (
+          <>
+            <button
+              type="button"
+              className="rounded-[5px] bg-gray-500 px-[30px] py-[16px] text-theme-white"
+              onClick={handlePrevStep}
+            >
+              Deploy Pool
+            </button>
+            <button
+              type="button"
+              className="rounded-[5px] bg-gray-500 px-[30px] py-[16px] text-theme-white"
+              onClick={handleNextStep}
+            >
+              Next
+            </button>
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <button
+              type="button"
+              className="rounded-[5px] bg-gray-500 px-[30px] py-[16px] text-theme-white"
+              onClick={handlePrevStep}
+            >
+              Integration Test
+            </button>
+            <button
+              type="button"
+              className="rounded-[5px] bg-gray-500 px-[30px] py-[16px] text-theme-white"
+              onClick={handleNextStep}
+            >
+              Next
+            </button>
+          </>
+        );
+      case 4:
+        return (
+          <button
+            type="button"
+            className="rounded-[5px] bg-gray-500 px-[30px] py-[16px] text-theme-white"
+            onClick={handlePrevStep}
+          >
+            Go to dashboard
+          </button>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div className="flex h-full w-full flex-col ">
+    <div className="flex h-full w-full flex-col">
       <Header />
       <SubHeader />
-      <div className="flex flex-col gap-[60px] pl-[60px]">
-        <p className=" pt-[30px] text-20/semi-bold">Register Product Information</p>
-        <RegisterProducts step={2} />
+
+      <div className={clsx("flex flex-row gap-[60px] pl-[60px]")}>
+        <RegisterProducts step={step} />
+        <div className="mt-[30px] h-full w-[2px] bg-gray-200" />
+        <div className="mt-[30px] flex h-full  w-full flex-col gap-10">
+          <div className="flex flex-col gap-5">
+            <p className="text-[24px] font-semibold">{title}</p>
+            <p className="text-16/regular">{subtitle}</p>
+            <div className="flex flex-col gap-5">{renderContentsField()}</div>
+          </div>
+          <div className="flex justify-end">{renderButtons()}</div>
+        </div>
       </div>
     </div>
   );
