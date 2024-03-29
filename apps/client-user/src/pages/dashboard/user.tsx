37e54af2bfcbe43d7df004fccb4902fac6b41bd0
DoYam <doniang1123@gmail.com>
2024-03-30 07:13:12 +0900

    Chore: Clean User Page

--- apps/client-user/src/pages/dashboard/user.tsx
+++ apps/client-user/src/pages/dashboard/user.tsx
@@ -1,13 +1,7 @@
-import RegisterProducts from "../../components/Register/RegisterProducts/RegisterProducts";
-
 export const User = () => {
   return (
     <div>
       <h1>User</h1>
-      <RegisterProducts step={1} />
-      <RegisterProducts step={2} />
-      <RegisterProducts step={3} />
-      <RegisterProducts step={4} />
     </div>
   );
 };
